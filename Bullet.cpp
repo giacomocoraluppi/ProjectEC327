19830d7bbae30daccc4c9096fc778e4bea4c1a51
liaophie <90203309+liaophie@users.noreply.github.com>
2021-12-06 15:41:46 -0500

    updated to add more functions

--- Bullet.cpp
+++ Bullet.cpp
@@ -17,8 +17,19 @@ Bullet::Bullet(int xIn, int yIn, int angle)
     yTraj = speed * sin(double(angle)*3.14159/180.0);
 }
 
-void Bullet::update()
+/*
+Bullet::~Bullet()
 {
+}
+
+*/
+
+void Bullet::update()//should includes a time elapsedTime
+{
+	/*
+	xLocation += elapsedTime * xTraj;
+	yLocation += elapsedTime * yTraj;
+	*/
         xLocation = xLocation + xTraj;
         yLocation = yLocation + xTraj;
         
@@ -26,6 +37,19 @@ void Bullet::update()
         //cout << "xLocation: " << xLocation << endl;
         //cout << "yLocation: " << yLocation << endl;
 }
+
+/*
+bool Bullet::IsOut()
+{
+//RESOLUTION_X and RESOLUTION_Y represents the edge of the window
+	// Returns true if the projectile is out of the screen area so we can remove it
+	if (xLocaiton < -10 || xLocation > RESOLUTION_X + 10 || yLocation < -10 || yLocation > RESOLUTION_Y + 10)
+	{
+		return true;
+	}
+	return false;
+}
+*/
 
 void Bullet::loseLives()
 {
